5906d1e37593e77f0e38b3b8d267f1d60131fe95
Vander Gerald <vander.gerald@ui.ac.id>
2024-04-18 21:45:29 +0700

    Initialize Java project for request barang

---
+++ src/main/java/com/adpro/requestbarang/RequestbarangApplication.java
@@ -0,0 +1,13 @@
+package com.adpro.requestbarang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestbarangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestbarangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adpro/requestbarang/RequestbarangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adpro.requestbarang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestbarangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
